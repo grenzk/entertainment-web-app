c3445404039214374edec307a48ef5d1bb368aa9
grenzk <74033991+grenzk@users.noreply.github.com>
2024-02-10 16:12:40 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Welcome to Vue!</h1>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about" style="margin-left: 1rem">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
