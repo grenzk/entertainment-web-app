0d19ca3f7cefad7117cccf02318a03cf63550a0e
grenzk <74033991+grenzk@users.noreply.github.com>
2024-02-18 13:44:52 +0800

    Install and set up quasar

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from '@/App.vue'
 import { router } from '@/router'
 
+import 'quasar/src/css/index.sass'
 import '@/assets/scss/styles.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {})
 
 app.mount('#app')
