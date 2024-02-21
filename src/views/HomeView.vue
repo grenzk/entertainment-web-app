995b7e1a93839f5a3c1f58d6463d7d608d60d9dd
grenzk <74033991+grenzk@users.noreply.github.com>
2024-02-21 13:52:31 +0800

    Refactor css for readability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,300 +7,300 @@ import EmptyBookmarkIcon from '@/components/icons/EmptyBookmarkIcon.vue'
 <template>
   <SearchInput />
   <div class="trending">
-    <h2 class="section-title">Trending</h2>
+    <h2 class="section-title l-container">Trending</h2>
 
-    <div class="media-scroller l-grid snaps-inline">
-      <div class="media-element">
+    <div class="trending-media-scroller l-grid snaps-inline">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
-      <div class="media-element">
+      <div class="media">
         <img
           class="media-img"
           src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="gradient-overlay"></div>
+        <div class="media-gradient-overlay"></div>
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>PG</small>
           </div>
-          <h3 class="media-title">Beyond Earth</h3>
+          <h3 class="media-info-title">Beyond Earth</h3>
         </div>
       </div>
     </div>
   </div>
-  <div class="recommended">
+  <div class="recommended l-container">
     <h2 class="section-title">Recommended for you</h2>
 
-    <div class="recommended-content l-container l-grid">
-      <div class="media-element recommended-media">
+    <div class="recommended-group l-grid">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
-      <div class="media-element recommended-media">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
-      <div class="media-element recommended-media">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
-      <div class="media-element recommended-media">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
-      <div class="media-element recommended-media">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
-      <div class="media-element recommended-media">
+      <div class="media media-recommended">
         <img
           class="media-img"
           src="../assets/img/thumbnails/the-great-lands/regular/large.jpg"
           alt="Beyond Earth"
         />
 
-        <div class="bookmark l-flex"><EmptyBookmarkIcon /></div>
+        <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
 
         <div class="media-info">
-          <div class="tags l-flex">
+          <div class="media-info-tags l-flex">
             <small>2019</small>
-            <small class="dot"></small>
-            <small class="category-icon"><MovieCategoryIcon /></small>
+            <small></small>
+            <small><MovieCategoryIcon /></small>
             <small>Movie</small>
-            <small class="dot"></small>
+            <small></small>
             <small>E</small>
           </div>
-          <h3 class="media-title">The Great Lands</h3>
+          <h3 class="media-info-title">The Great Lands</h3>
         </div>
       </div>
     </div>
@@ -321,102 +321,108 @@ import EmptyBookmarkIcon from '@/components/icons/EmptyBookmarkIcon.vue'
   font-size: var(--font-size-xl);
   line-height: revert;
   letter-spacing: -0.31px;
-  margin: 0 0 1rem 1rem;
+  margin-bottom: var(--space-content-gap);
 }
 
-.media-scroller {
-  scrollbar-width: none;
-  padding: 0 1rem;
-  grid-auto-flow: column;
-  gap: 1rem;
-  overflow-x: auto;
-  overscroll-behavior-inline: contain;
-  width: 100vw;
-}
-
-.media-element {
+.media {
   overflow: hidden;
   border-radius: var(--border-radius-xs);
   position: relative;
-  width: 240px;
-  height: 140px;
+
+  &-img {
+    object-fit: cover;
+    height: 100%;
+  }
+
+  &-info {
+    position: absolute;
+    left: 1rem;
+    bottom: 1rem;
+    z-index: 1;
+
+    &-tags {
+      gap: var(--space-media-info-gap);
+      color: hsla(0, 0%, 100%, 0.75);
+      align-items: center;
+
+      small:nth-child(2),
+      small:nth-child(5) {
+        width: 0.188rem;
+        height: 0.188rem;
+        background-color: hsla(0, 0%, 100%, 0.75);
+        border-radius: 50%;
+      }
+
+      small:nth-child(3) {
+        height: 0.75rem;
+      }
+    }
+
+    &-title {
+      font-size: var(--font-size-s);
+      line-height: revert;
+    }
+  }
+
+  &-gradient-overlay {
+    position: absolute;
+    background: linear-gradient(to bottom, hsla(0, 0%, 0%, 0), hsla(0, 0%, 0%, 0.5));
+    width: 100%;
+    height: 50%;
+    bottom: 0;
+    overflow: hidden;
+  }
+
+  &-bookmark-button {
+    background-color: hsla(223, 30%, 9%, 0.5);
+    border-radius: var(--border-radius-round);
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    right: 0.5rem;
+    top: 0.5rem;
+  }
 }
 
-.media-info {
-  position: absolute;
-  left: 1rem;
-  bottom: 1rem;
-  z-index: 1;
+.trending {
+  &-media-scroller {
+    scrollbar-width: none;
+    padding: 0 1rem;
+    grid-auto-flow: column;
+    grid-auto-columns: 15rem;
+    gap: 1rem;
+    overflow-x: auto;
+    overscroll-behavior-inline: contain;
+    width: 100vw;
+    height: 8.75rem;
+  }
 }
 
-.media-img {
-  object-fit: cover;
-  height: 100%;
+.recommended {
+  &-group {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.938rem;
+  }
 }
 
-.tags {
-  gap: var(--space-media-info-gap);
-  align-items: center;
-}
-
-.category-icon {
-  height: 12px;
-}
-
-.dot {
-  width: 3px;
-  height: 3px;
-  background-color: white;
-  border-radius: 50%;
-}
-
-.media-title {
-  font-size: var(--font-size-s);
-  line-height: revert;
-}
-
-.gradient-overlay {
-  position: absolute;
-  background: linear-gradient(to bottom, hsla(0, 0%, 0%, 0), hsla(0, 0%, 0%, 0.5));
-  width: 100%;
-  height: 50%;
-  bottom: 0;
-  overflow: hidden;
-}
-
-.bookmark {
-  background-color: hsla(223, 30%, 9%, 0.5);
-  border-radius: var(--border-radius-round);
-  justify-content: center;
-  align-items: center;
-  width: 32px;
-  height: 32px;
-  position: absolute;
-  right: 0.5rem;
-  top: 0.5rem;
-}
-
-.recommended-content {
-  grid-template: 154px 154px / 1fr 1fr;
-  gap: 1rem;
-}
-
-.media-element.recommended-media {
-  width: 164px;
-  height: 110px;
+.media.media-recommended > .media {
   overflow: visible;
 
-  .media-img {
+  &-img {
     border-radius: var(--border-radius-xs);
     margin-bottom: 0.5rem;
+    height: 6.875rem;
+    width: 100%;
   }
 
-  .media-info {
+  &-info {
     position: static;
-  }
 
-  .tags {
-    gap: 0.375rem;
+    &-tags {
+      gap: 0.375rem;
+    }
   }
 }
 </style>
