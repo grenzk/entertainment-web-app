a73ca5d79a09f1d760a82956e0f237f733627f28
grenzk <74033991+grenzk@users.noreply.github.com>
2024-02-21 14:27:21 +0800

    Componentize media content

---
+++ src/components/MediaContent.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import EmptyBookmarkIcon from '@/components/icons/EmptyBookmarkIcon.vue'
+import MovieCategoryIcon from '@/components/icons/MovieCategoryIcon.vue'
+</script>
+
+<template>
+  <div class="media">
+    <img
+      class="media-img"
+      src="../assets/img/thumbnails/beyond-earth/trending/large.jpg"
+      alt="Beyond Earth"
+    />
+
+    <div class="media-gradient-overlay"></div>
+    <div class="media-bookmark-button l-flex"><EmptyBookmarkIcon /></div>
+
+    <div class="media-info">
+      <div class="media-info-tags l-flex">
+        <small>2019</small>
+        <small></small>
+        <small><MovieCategoryIcon /></small>
+        <small>Movie</small>
+        <small></small>
+        <small>PG</small>
+      </div>
+      <h3 class="media-info-title">Beyond Earth</h3>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.media {
+  overflow: hidden;
+  border-radius: var(--border-radius-xs);
+  position: relative;
+
+  &-img {
+    object-fit: cover;
+    height: 100%;
+  }
+
+  &-info {
+    position: absolute;
+    left: 1rem;
+    bottom: 1rem;
+    z-index: 1;
+
+    &-tags {
+      gap: var(--space-media-info-gap);
+      color: hsla(0, 0%, 100%, 0.75);
+      align-items: center;
+
+      small:nth-child(2),
+      small:nth-child(5) {
+        width: 0.188rem;
+        height: 0.188rem;
+        background-color: hsla(0, 0%, 100%, 0.75);
+        border-radius: 50%;
+      }
+
+      small:nth-child(3) {
+        height: 0.75rem;
+      }
+    }
+
+    &-title {
+      font-size: var(--font-size-s);
+      line-height: revert;
+    }
+  }
+
+  &-gradient-overlay {
+    position: absolute;
+    background: linear-gradient(to bottom, hsla(0, 0%, 0%, 0), hsla(0, 0%, 0%, 0.5));
+    width: 100%;
+    height: 50%;
+    bottom: 0;
+    overflow: hidden;
+  }
+
+  &-bookmark-button {
+    background-color: hsla(223, 30%, 9%, 0.5);
+    border-radius: var(--border-radius-round);
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    right: 0.5rem;
+    top: 0.5rem;
+  }
+}
+
+.media.media-recommended > .media {
+  overflow: visible;
+
+  &-img {
+    border-radius: var(--border-radius-xs);
+    margin-bottom: 0.5rem;
+    height: 6.875rem;
+    width: 100%;
+  }
+
+  &-gradient-overlay {
+    display: none;
+  }
+
+  &-info {
+    position: static;
+
+    &-tags {
+      gap: 0.375rem;
+    }
+  }
+}
+</style>
