8af6e3cdd200daa44b55dffc5318234f17e11b1e
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-09 17:14:37 +0800

    Use implicit grid

--- src/utils/grid.ts
+++ src/utils/grid.ts
@@ -1,14 +1,14 @@
 const gridContainer = document.getElementById('app') as HTMLElement
 
 export const changeGridRows = () => {
-  gridContainer.style.gridTemplateRows = '5rem 3rem auto'
+  gridContainer.style.gridTemplateRows = '5rem 3rem'
 
   if (window.innerWidth >= 768) {
-    gridContainer.style.gridTemplateRows = '8.063rem 4.063rem auto'
+    gridContainer.style.gridTemplateRows = '8.063rem 4.063rem'
   }
 
   if (window.innerWidth >= 1024) {
-    gridContainer.style.gridTemplateRows = '8.125rem auto'
+    gridContainer.style.gridTemplateRows = '8.125rem'
   }
 }
 
