59da94eba7766277e3f8c6aed5d23bfb4367c21a
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-04 11:28:50 +0800

    Fetch data from the back-end

--- src/stores/media.ts
+++ src/stores/media.ts
@@ -1,9 +1,10 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import data from '@/assets/data.json'
+import Axios from 'axios'
 
 export const useMediaStore = defineStore('media', () => {
-  const shows = ref<MediaItem[]>(data)
+  const data = ref<MediaItem[]>([])
+  const shows = ref<MediaItem[]>([])
   const userInput = ref('')
 
   const filteredShows = computed({
@@ -19,9 +20,21 @@ export const useMediaStore = defineStore('media', () => {
   })
 
   const resetShows = () => {
-    shows.value = data
+    shows.value = data.value
     userInput.value = ''
   }
 
-  return { shows, userInput, filteredShows, resetShows }
+  const fetchMediaData = async () => {
+    try {
+      const response = await Axios.get('http://127.0.0.1:3000/api/v1/media')
+      data.value = response.data
+      shows.value = response.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  fetchMediaData()
+
+  return { shows, userInput, filteredShows, resetShows, fetchMediaData }
 })
