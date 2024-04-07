56e516efc6f65556b7ba4a4a6245449cfedf1050
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-07 14:57:16 +0800

    Filter and display bookmarked shows

--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, onUnmounted } from 'vue'
+import { onBeforeUnmount, onMounted, onUnmounted, ref, computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useMediaStore } from '@/stores/media'
+import Axios from 'axios'
 import { changeGridRows, resetGrid } from '@/utils/grid'
 
 import MediaSection from '@/components/MediaSection.vue'
@@ -10,20 +11,30 @@ const store = useMediaStore()
 const { shows, userInput } = storeToRefs(store)
 const { resetShows } = store
 
-const bookmarkedMovies = shows.value.filter((show) => {
-  return show.isBookmarked && show.category === 'Movie'
-})
-const bookmarkedTvSeries = shows.value.filter((show) => {
-  return show.isBookmarked && show.category === 'TV Series'
-})
+const bookmarks = ref<number[]>([])
 
-const bookmarkedShows = shows.value.filter((media) => media.isBookmarked)
+const fetchBookmarks = async () => {
+  try {
+    const response = await Axios.get('http://127.0.0.1:3000/api/v1/bookmarks')
+    bookmarks.value = response.data.map((item: { medium_id: number }) => item.medium_id)
+    shows.value = shows.value.filter((show) => bookmarks.value.includes(show.id))
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 onMounted(() => {
   changeGridRows()
+  fetchBookmarks()
   window.addEventListener('resize', changeGridRows)
+})
 
-  shows.value = bookmarkedShows
+const bookmarkedMovies = computed(() => {
+  return shows.value.filter((show) => show.category === 'Movie')
+})
+
+const bookmarkedTvSeries = computed(() => {
+  return shows.value.filter((show) => show.category === 'TV Series')
 })
 
 onBeforeUnmount(() => resetShows())
