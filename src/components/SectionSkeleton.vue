8f030f3ab9f2e743e12933c47b9e57aba26341b5
grenzk <74033991+grenzk@users.noreply.github.com>
2024-05-21 17:30:06 +0800

    Improve skeleton loader

--- src/components/SectionSkeleton.vue
+++ src/components/SectionSkeleton.vue
@@ -3,23 +3,30 @@
     <QSkeleton class="section-title custom-skeleton" width="150px" animation="fade" />
 
     <div class="media-library-group l-grid">
-      <QSkeleton
-        class="media-skeleton custom-skeleton"
-        v-for="num in 8"
-        :key="num"
-        animation="fade"
-      />
+      <div class="media" v-for="num in 8" :key="num">
+        <QSkeleton class="media-skeleton custom-skeleton" animation="fade" />
+
+        <div class="media-info">
+          <QSkeleton class="media-skeleton-tags custom-skeleton" animation="fade" />
+          <QSkeleton class="custom-skeleton" width="100px" animation="fade" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-.media-skeleton {
-  height: 6.875rem !important;
-}
-
 .custom-skeleton {
   background: var(--color-primary-semi-dark-blue) !important;
   border-radius: var(--border-radius-s) !important;
+}
+
+.media-skeleton {
+  height: 6.875rem !important;
+  margin-bottom: 0.5rem;
+}
+
+.media-skeleton-tags {
+  margin-bottom: 0.5rem;
 }
 </style>
